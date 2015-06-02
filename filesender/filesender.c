68e84f64bf506be8c78ae3858e75d57fbb3f2f53
Kuznetsov Nikolay <kna.rus@gmail.com>
2015-06-02 13:41:09 +0300

    more error handling

--- filesender/filesender.c
+++ filesender/filesender.c
@@ -5,23 +5,24 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 4096
+#define LISTEN_QUEUE 5
 
 int main(int argc, char** argv) {
 	if(argc < 3) {
 		write_(STDOUT_FILENO, "Usage: <port> <file>\n", 21);
 		return 10;
 	}
-	unsigned short port = atoi(argv[1]);
+	signal(SIGCHLD, SIG_IGN); // auto-collect all zombies (allowed since POSIX.1-2001)
 	struct addrinfo *localhost;
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof(struct addrinfo));
-	if(getaddrinfo("localhost", 0, 0, &localhost)) {
+	if(getaddrinfo("localhost", argv[1], 0, &localhost)) {
 		return 1;
 	}
 	int sock = socket(localhost->ai_family, SOCK_STREAM, 0);
@@ -31,6 +32,9 @@ int main(int argc, char** argv) {
 	if(bind(sock, localhost->ai_addr, localhost->ai_addrlen)) {
 		return 3;
 	}
+	if(listen(sock, LISTEN_QUEUE)) {
+		return 5;
+	}
 	freeaddrinfo(localhost);
 	while(1) {
 		int cli = accept(sock, 0, 0);
@@ -38,25 +42,34 @@ int main(int argc, char** argv) {
 			return 4;
 		}
 		pid_t pid = fork();
+		if(pid < 0) {
+			return 5;
+		}
 		if(pid) {
 			close(cli);
 			continue;
 		} else {
+			close(sock);
 			int file = open(argv[2], O_RDONLY);
 			if(file < 0) {
 				return 1;
 			}
 			buf_t *buf = buf_new(BUFFER_SIZE);
-			int bfr = buf_fill(file, buf, buf_capacity(buf));
-			if(bfr < 0) {
-				if(buf_flush(cli, buf, buf_size(buf)) < 0)
-					return 1;
+			if(!buf) {
 				return 2;
 			}
-			if(bfr == 0)
-				break;
-			if(buf_flush(cli, buf, buf_size(buf)) < 0)
-				return 3;
+			while(1) {
+				int bfr = buf_fill(file, buf, buf_capacity(buf));
+				if(bfr < 0) {
+					if(buf_flush(cli, buf, buf_size(buf)) < 0)
+						return 3;
+					return 4;
+				}
+				if(bfr == 0)
+					return 0;
+				if(buf_flush(cli, buf, buf_size(buf)) < 0)
+					return 5;
+			}
 		}
 	}
 	return 0;
